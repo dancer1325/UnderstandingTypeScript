068677617a9f80b4fa8543dbdce43a3e6832489d
dancer1325 <alfredotic0809@gmail.com>
2022-06-20 17:27:14 +0200

    (chapter 14): add commentaries about interfaces (Section 5)

--- cl-interfaces-15-optional-properties/src/app.ts
+++ cl-interfaces-15-optional-properties/src/app.ts
@@ -1,19 +1,43 @@
+// interface vs type
+// 1) type    === custom types
 // type AddFn = (a: number, b: number) => number;
+// 2) interface
 interface AddFn {
-  (a: number, b: number): number;
+  (a: number, b: number): number;   // anonymous function (===Unnamed)
 }
 
+// allows describing the structure
 let add: AddFn;
 
+// and then, you indicate the implementation
 add = (n1: number, n2: number) => {
   return n1 + n2;
 };
 
 interface Named {
+  // modifiers in an interface
+  // public name?: string;      // not allowed
+  // private name?: string;     // not allowed
+  readonly name?: string;       // any object based on this interface, this property just established at the beginning
+  outputName?: string;
+}
+
+
+let named: Named;
+// Declare the object as commonly
+named = {
+  name:'Alfredo',
+  outputName: 'Alfredo'
+};
+
+// Previous behavior could be replaced by custom type, instead of interface
+type NamedAsType = {
   readonly name?: string;
   outputName?: string;
 }
 
+// extends from another interface
+// several interfaces can be extended from
 interface Greetable extends Named {
   greet(phrase: string): void;
 }
